dc9db875a2b72f2f9f88bb75871156e74ef59831
gis-14240514 <gaojinzecs@163.com>
2026-03-09 22:20:41 +0000

    LCM workflow

--- spatial ecology.R
+++ spatial ecology.R
@@ -22,3 +22,29 @@ dir.create(out_dir_w3, showWarnings=FALSE, recursive=TRUE)
 # Terra temp (avoid RAM spikes)
 dir.create(file.path(data_dir, "terra_tmp"), showWarnings=FALSE)
 terra::terraOptions(tempdir=file.path(data_dir, "terra_tmp"), memfrac=0.6)
+# Load LCM, project points, crop raster to points (+5 km)
+LCM <- terra::rast(lcm_file)
+if (terra::nlyr(LCM) > 1) LCM <- LCM[[1]]
+
+meles_proj <- sf::st_transform(meles_ll, terra::crs(LCM))
+
+xy <- sf::st_coordinates(meles_proj)
+stopifnot(nrow(xy) > 0)
+
+ext_new <- terra::ext(min(xy[,1])-5000, max(xy[,1])+5000,
+                      min(xy[,2])-5000, max(xy[,2])+5000)
+
+LCM_crop <- terra::crop(LCM, ext_new)
+# Aggregate LCM to 100 m first (modal)
+# LCM is 25 m, fact=4 gives 100 m
+LCM100 <- terra::aggregate(LCM_crop, fact = 4, fun = "modal")
+LCM_fac <- terra::as.factor(LCM100)
+# Reclassify to broadleaf binary raster 
+broadleaf_code <- 2  # TODO: change after checking levels(LCM_fac) if needed
+
+lv <- levels(LCM_fac)[[1]]
+codes <- lv[,1]
+reclass <- ifelse(codes == broadleaf_code, 1, 0)
+RC <- apply(cbind(codes, reclass), 2, as.numeric)
+
+broadleaf <- terra::classify(LCM_fac, RC)
